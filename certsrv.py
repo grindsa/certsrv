a9bd8faabe1fdf64e99a113571d3216959a9b041
grindsa <grindelsack@gmail.com>
2024-04-24 20:19:11 +0200

    kerberos support to address grindsa/acme2certifier#153

--- certsrv.py
+++ certsrv.py
@@ -56,7 +56,7 @@ class Certsrv(object):
         username: The username for authentication.
         password: The password for authentication.
         auth_method: The chosen authentication method. Either 'basic' (the default),
-            'ntlm' or 'cert' (SSL client certificate).
+            'ntlm', 'cert' (SSL client certificate) or 'gssapi' (GSSAPI, Kerberos)
         cafile: A PEM file containing the CA certificates that should be trusted.
         timeout: The timeout to use against the CA server, in seconds.
             The default is 30.
@@ -99,7 +99,9 @@ class Certsrv(object):
             from requests_ntlm import HttpNtlmAuth
 
             self.session.auth = HttpNtlmAuth(username, password)
-        elif self.auth_method == "kerberos":
+        elif self.auth_method == "cert":
+            self.session.cert = (username, password)
+        elif self.auth_method == "gssapi":
             from requests_gssapi import HTTPSPNEGOAuth
             import gssapi
             oid = '1.3.6.1.5.5.2'  # SPNEGO
@@ -110,8 +112,6 @@ class Certsrv(object):
                 usage="initiate",
             )
             self.session.auth = HTTPSPNEGOAuth(creds=cred.creds)
-        elif self.auth_method == "cert":
-            self.session.cert = (username, password)
         else:
             self.session.auth = (username, password)
 
@@ -334,7 +334,7 @@ class Certsrv(object):
             username: The username for authentication.
             password: The password for authentication.
         """
-        if self.auth_method in ("ntlm", "cert"):
+        if self.auth_method in ("ntlm", "cert", "gssapi"):
             # NTLM and SSL is connection based,
             # so we need to close the connection
             # to be able to re-authenticate
@@ -371,7 +371,7 @@ def get_cert(server, csr, template, username, password, encoding="b64", **kwargs
         encoding: The desired encoding for the returned certificate.
             Possible values are 'bin' for binary and 'b64' for Base64 (PEM).
         auth_method: The chosen authentication method. Either 'basic' (the default),
-            'ntlm' or 'cert' (ssl client certificate).
+            'ntlm', 'cert' (ssl client certificate) or 'gssapi' (GSSAPI, Kerberos).
         cafile: A PEM file containing the CA certificates that should be trusted.
 
     Returns:
@@ -409,7 +409,7 @@ def get_existing_cert(server, req_id, username, password, encoding="b64", **kwar
         encoding: The desired encoding for the returned certificate.
             Possible values are 'bin' for binary and 'b64' for Base64 (PEM).
         auth_method: The chosen authentication method. Either 'basic' (the default),
-            'ntlm' or 'cert' (ssl client certificate).
+            'ntlm', 'cert' (ssl client certificate) or 'gssapi' (GSSAPI, Kerberos).
         cafile: A PEM file containing the CA certificates that should be trusted.
 
     Returns:
@@ -442,7 +442,7 @@ def get_ca_cert(server, username, password, encoding="b64", **kwargs):
         encoding: The desired encoding for the returned certificate.
             Possible values are 'bin' for binary and 'b64' for Base64 (PEM).
         auth_method: The chosen authentication method. Either 'basic' (the default),
-            'ntlm' or 'cert' (ssl client certificate).
+            'ntlm', 'cert' (ssl client certificate) or 'gssapi' (GSSAPI, Kerberos).
         cafile: A PEM file containing the CA certificates that should be trusted.
 
     Returns:
@@ -471,7 +471,7 @@ def get_chain(server, username, password, encoding="bin", **kwargs):
         encoding: The desired encoding for the returned certificates.
             Possible values are 'bin' for binary and 'b64' for Base64 (PEM).
         auth_method: The chosen authentication method. Either 'basic' (the default),
-            'ntlm' or 'cert' (ssl client certificate).
+            'ntlm', 'cert' (ssl client certificate) or 'gssapi' (GSSAPI, Kerberos).
         cafile: A PEM file containing the CA certificates that should be trusted.
 
     Returns:
@@ -498,7 +498,7 @@ def check_credentials(server, username, password, **kwargs):
         username: The username for authentication.
         pasword: The password for authentication.
         auth_method: The chosen authentication method. Either 'basic' (the default),
-            'ntlm' or 'cert' (ssl client certificate).
+            'ntlm', 'cert' (ssl client certificate) or 'gssapi' (GSSAPI, Kerberos).
         cafile: A PEM file containing the CA certificates that should be trusted.
 
     Returns:
